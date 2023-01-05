bf5057171a27ef490e9f38cca64de4463ca83964
kennethKPT <120914721+kennethKPT@users.noreply.github.com>
2023-01-05 17:06:50 -0600

    Updated config.js setting

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_0hBmTOkrC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6un1d2hv0t5ava9u32ec0tk27k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_kSPEUsl7V', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3goktdou7grjkatcd052rgade1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://53pk21cg79.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
